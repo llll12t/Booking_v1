c94c5f9d01e18e6fca941d8e5fd7e22e9c746722
llll12t <147978645+llll12t@users.noreply.github.com>
2025-03-07 11:38:17 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,8 @@
 <script>
-//ตั้งค่าทั้งหมด
-
     const DETAILS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyIzvRzAUYK-LSa_3kOHCJ7SLahcdh2f0TU2Xd0GtHreWFR_7y0uVrql203fuhAWH0G/exec';
     const MEMBER_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyIzvRzAUYK-LSa_3kOHCJ7SLahcdh2f0TU2Xd0GtHreWFR_7y0uVrql203fuhAWH0G/exec';
     const LIFF_ID = '2006029649-kVZ9XqOV';  // ID สำหรับ LIFF
     const LIFF_ID2 = '2006029649-kVZ9XqOV'; // ID สำหรับ LIFF
     const LIFF_ID3 = '2006029649-kVZ9XqOV'; // ID สำหรับ LIFF
     const LIFF_ID4 = '2006029649-kVZ9XqOV'; // ID สำหรับ LIFF
-
 </script>
